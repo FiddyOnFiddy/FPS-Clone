3f4841f6c18c5087786e1aa3cabb056dcccb16f9
MattBee2k2 <matthewbiggins@hotmail.co.uk>
2018-08-31 00:23:00 +0100

    sa

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float mouseX;
+    public float mouseY;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        CameraControl();
+	}
+
+    void CameraControl()
+    {
+        player.transform.eulerAngles += new Vector3(-mouseY, transform.eulerAngles.y, 0.0f);
+        transform.eulerAngles += new Vector3(transform.eulerAngles.x, mouseX, 0.0f);
+    }
+}
---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    public float movementSpeed = 10.0f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        PlayerMovement();
+	}
+
+    void PlayerMovement()
+    {
+        rb.velocity = new Vector3(horizontalMovement * movementSpeed * Time.fixedDeltaTime, rb.velocity.y, verticalMovement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
